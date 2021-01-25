40a84c40e83d1f54c896a19cee7356dcf5303873
likang <k.li@bmi-tech.com>
2021-01-25 16:54:26 +0800

    update hevc parameters

--- modules/hevc.py
+++ modules/hevc.py
@@ -7,10 +7,12 @@ from .core import FUNCTION_REGISTER, Operator
 class HEVC(Operator):
     """reference：https://www.cnblogs.com/blackhumour2018/p/9427665.html
     """
-    def __init__(self, Q, keyInterval, bppRef):
+    def __init__(self, crf,keyint=None,preset=None, tune=None, bppRef='input'):
         super(HEVC, self).__init__()
-        self.Q = int(Q)
-        self.keyInterval = int(keyInterval)
+        self.crf = crf
+        self.keyint = keyint
+        self.preset = preset
+        self.tune = tune
         self.bppRef = bppRef
 
     def operate(self, input, output):
@@ -18,11 +20,19 @@ class HEVC(Operator):
         if not os.path.exists(output):
             os.makedirs(output)
         w, h, fps = self.extractParameters(input)
-        tmp_output = os.path.join(output, 'tmp.h265')
+        tmp_output = os.path.join(output, 'tmp.mkv')
         #ffmpeg -framerate 50.0 -s 960x540 -i 960x540_50.0fps.yuv -r 50.0 -vcodec libx265 -x265-params "keyint=10:min-keyint=5:crf=20:no-scenecut=1" -f hevc out.h265
-        cmd = 'ffmpeg -s {}x{} -framerate {} -i {} -r {} -vcodec libx265 \
-            -x265-params keyint={}:min-keyint={}:crf={}:no-scenecut=1 -f hevc {} -y -hide_banner'.format(
-            w, h, fps, input,fps, self.keyInterval, self.keyInterval, self.Q , tmp_output)
+        # cmd = 'ffmpeg -s {}x{} -framerate {} -i {} -r {} -vcodec libx265 \
+        #     -x265-params keyint={}:min-keyint={}:crf={}:no-scenecut=1 -f hevc {} -y -hide_banner'.format(
+        #     w, h, fps, input,fps, self.keyInterval, self.keyInterval, self.Q , tmp_output)
+        cmd = 'ffmpeg -s {}x{} -framerate {} -i {} -r {} -c:v libx265'.format(w,h, fps, input,fps)
+        cmd += ' -preset '+self.preset if self.preset else ''
+        cmd += ' -tune ' + self.tune if self.tune else ''
+        cmd += ' -x265-params crf={}'.format(self.crf)
+        cmd += ':keyint={}'.format(self.keyint) if self.keyint else ''
+        cmd += ' {} -y -hide_banner'.format(tmp_output)
+
+        # print(cmd)
         
         p = subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
         line_queue = deque(maxlen=2)
@@ -50,7 +60,7 @@ class HEVC(Operator):
                 bpp = self.calculateBPP(tmp_output, w_input, h_input, frames)
             newFileName = self.generateFileName(w,h,fps,bpp=round(bpp, 4), avgQP=QP, kbps=kbps)
             output = os.path.join(output,newFileName)
-            cmd = 'ffmpeg -framerate {} -i {} {} -hide_banner'.format(fps, tmp_output, output)
+            cmd = 'ffmpeg -i {} {} -hide_banner'.format(tmp_output, output)
             os.system(cmd)
             os.remove(tmp_output)
             print('HEVC finish:{}'.format(output))
