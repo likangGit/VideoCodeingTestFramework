a806253665290c5bb200c43b37b31bc9e0ba4dc7
likang <k.li@bmi-tech.com>
2021-05-13 14:41:52 +0800

    add config file path

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from modules.core import Exec
 import yaml
-with open('config.yaml', encoding='utf-8') as f:
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("config", type=str, help="config file ")
+args = parser.parse_args()
+with open(args.config, encoding='utf-8') as f:
     config = yaml.safe_load(f)
 print(config)
 Exec(config)
