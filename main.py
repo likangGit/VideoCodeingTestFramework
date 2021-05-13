ed7d6533f202f93fac58aaf94f98f1e9534646e7
likang <k.li@bmi-tech.com>
2021-05-13 15:24:27 +0800

    add new config file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("config", type=str, help="config file ")
 args = parser.parse_args()
+
 with open(args.config, encoding='utf-8') as f:
     config = yaml.safe_load(f)
 print(config)
